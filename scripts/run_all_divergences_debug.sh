f3be56c0e92d2f680fe15e2a1cf28ebe803778b1
Thang1703hrsh <ducthang170301@gmail.com>
2026-04-29 08:16:55 +0000

    fix f star

--- scripts/run_all_divergences_debug.sh
+++ scripts/run_all_divergences_debug.sh
@@ -41,7 +41,7 @@ N_EPOCHS=1
 SKIP_EXISTING="${SKIP_EXISTING:-0}"
 GPU_IDS="0,1,2,3,4,5,6,7"
 
-ALL_DIVERGENCES=(js kl wasserstein)
+ALL_DIVERGENCES=(js kl hellinger)
 
 if [ $# -ge 1 ]; then
     ALL_DIVERGENCES=("$1")
